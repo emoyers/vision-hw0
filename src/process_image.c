c152fffcfc445d9b21b6280efeef4eed9ecfbf5e
Emilio Moyers <emoyersb@gmail.com>
2019-01-27 23:57:11 -0500

    1 and 2 test passed

--- src/process_image.c
+++ src/process_image.c
@@ -7,12 +7,80 @@
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    return 0;
+    int size_column = im.w; 
+    int size_row = im.h;
+    int size_channel = im.c;
+
+    if (x >= size_column)
+    {
+        x = size_column - 1;
+    }
+    else if (x < 0)
+    {
+        x = 0;
+    }
+
+    if (y >= size_row)
+    {
+        y = size_row - 1;
+    }
+    else if (y < 0)
+    {
+        y = 0;
+    }
+
+    if (c >= size_channel)
+    {
+        c = size_channel - 1;
+    }
+    else if (c < 0)
+    {
+        c = 0;
+    }
+
+    float pixel = im.data[x+y*size_column+c*size_column*size_row];
+    return pixel;
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
+    int size_column = im.w; 
+    int size_row = im.h;
+    int size_channel = im.c;
+    _Bool error = 0;
+
+    if (x >= size_column)
+    {
+        error = 1;
+    }
+    else if (x < 0)
+    {
+        error = 1;
+    }
+
+    if (y >= size_row)
+    {
+        error = 1;
+    }
+    else if (y < 0)
+    {
+        error = 1;
+    }
+
+    if (c >= size_channel)
+    {
+        error = 1;
+    }
+    else if (c < 0)
+    {
+        error = 1;
+    }
+
+    if (error == 0)  
+    {
+        im.data[x+y*size_column+c*size_column*size_row] = v;
+    } 
 }
 
 image copy_image(image im)
